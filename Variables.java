68f0517cd683ed45d96ccb178bed5e80c0649d8f
Vahe <168283825+Svaveop@users.noreply.github.com>
2024-07-11 21:58:06 +0400

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        byte age;
+        age = 14;
+        System.out.println("age:" + age + ".");
+        age = 37;
+        System.out.println(age);
+        float num1 = 9.8f;
+        double num2 = 3.1415926535d;
+        char sir = '@'; //as symbol
+        String user_name = "Andrew#";
+        System.out.println(user_name);
+        boolean isHappy = true;
+
+    }
+    //hints about:
+    // byte (from -128 to 127) space needs 1 byte
+    // short (from -32k to 32k) space needs 2 byte
+    // int (from -2b to 2b) space needs 4 byte
+    // long (from -9q to 9q) space need 8 byte
+}
