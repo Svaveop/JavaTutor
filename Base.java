e2f2bcad60146d714ce90852a205da6e554deb51
Vahe <168283825+Svaveop@users.noreply.github.com>
2024-07-11 21:53:13 +0400

    Create Base.java

---
+++ Base.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi \n\t\"\\ guys");
+        System.out.print("Hello");
+        System.out.println("World");
+      // Comment - Ignored code
+    }
+}
