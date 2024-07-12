d486504f48724bf750903c9f01caf86eb5af3959
Vahe <168283825+Svaveop@users.noreply.github.com>
2024-07-12 21:10:22 +0400

    java.util.Scanner

---
+++ ScanUse.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int num1 = scan.nextInt();
+        byte num2 = scan.nextByte();
+        boolean b = scan.nextBoolean();
+        float f = scan.nextFloat();
+  }
+}
