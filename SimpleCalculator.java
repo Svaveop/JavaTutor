74da30262622b77004832d908d1dd72c055ef928
Vahe <168283825+Svaveop@users.noreply.github.com>
2024-07-13 23:39:44 +0400

    Update SimpleCalculator.java

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -1,19 +1,52 @@
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter 1st number: ");
+        int num1 = scanner.nextInt();
 
-        System.out.println("Enter first number: ");
-        float num1 = scan.nextFloat();
-        System.out.println("Enter second number: ");
-        float num2 = scan.nextFloat();
-        float res1 = num1 * num2;
-        float res2 = num1 / num2;
-        float res3 = num1 + num2;
-        float res4 = num1 - num2;
-        System.out.println("Resault: ");
-        System.out.println(res1 + "\n" + res2 +"\n" + res3 + "\n" + res4);
-  }
+        System.out.print("Enter 2nd number: ");
+        int num2 = scanner.nextInt();
+
+        int res;
+
+        System.out.print("Action: ");
+        String action = scanner.nextLine();
+        action = scanner.nextLine();
+
+        switch (action) {
+            case "+":
+                res = num1 + num2;
+                System.out.println("Answer: " + res);
+                break;
+            case "-":
+                res = num1 - num2;
+                System.out.println("Answer: " + res);
+                break;
+            case "*":
+                res = num1 * num2;
+                System.out.println("Answer: " + res);
+                break;
+            case "/":
+                if (num2 == 0){
+                    System.out.println("Can't divide by 0");
+                }else {
+                    res = num1 / num2;
+                    System.out.println("Answer: " + res);
+                }
+                break;
+                // and if you write "/" like ":"
+            case ":":
+                if (num2 == 0){
+                    System.out.println("Can't divide by 0");
+                }else {
+                    res = num1 / num2;
+                    System.out.println("Answer: " + res);
+                }
+                break;
+            default:
+                System.out.println("You did something wrong");
+        }
+    }
 }
