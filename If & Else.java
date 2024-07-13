0baeb7cff7ecb95ea4fde740013fc14df0c0391a
Vahe <168283825+Svaveop@users.noreply.github.com>
2024-07-13 23:00:04 +0400

    Update If & Else.java

--- If & Else.java
+++ If & Else.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         boolean HasCar = false;
@@ -22,6 +24,19 @@ public class Main {
             System.out.println("Syntax Custom Error");
         } else if(a < b) {
             System.out.println("x(45) < y(55)");
+        }
+
+        // More practic use of If & Else
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        byte age = scanner.nextByte();
+        System.out.print("Enter your password: ");
+        String pass = scanner.nextLine();
+        if (age >= 16 && pass.equals("123")){
+            System.out.println("Welcome");
+        } else {
+            System.out.println("Access declined");
         }
         //also possible without else (only if & else if)
         //we can put in if another if or in else if another else if
