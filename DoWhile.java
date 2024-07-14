5f25c8540b1a0dd258f29a98b1734cdfe436780f
Vahe <168283825+Svaveop@users.noreply.github.com>
2024-07-14 21:36:31 +0400

    Create DoWhile.java

---
+++ DoWhile.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int k = 100;
+    do {
+        System.out.println("Element: " + k);
+        k*= 100;
+    } while(k < 10); 
+    } /* Like the same while but do while
+     does action 1 time and then checks condition
+     here the condition is: k < 10 */
+} 
