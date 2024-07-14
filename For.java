9122ae22c480d46bd02774215f4e7fc6ff820fa4
Vahe <168283825+Svaveop@users.noreply.github.com>
2024-07-14 20:40:18 +0400

    Create For.java

---
+++ For.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+    for(float i = 100; i >= 5; i /= 2){
+        System.out.println("Element: " + i);
+    }
+    for(short j = 10; j < 30; j++){
+        System.out.println("Answer: " + j);
+    }
+    }
+}
