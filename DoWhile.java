8ba1894f1e4997f0a704c212bf9016124881ea0e
Vahe <168283825+Svaveop@users.noreply.github.com>
2024-07-14 21:39:05 +0400

    Update DoWhile.java

--- DoWhile.java
+++ DoWhile.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int k = 100;
+        int k = 0;
     do {
         System.out.println("Element: " + k);
-        k*= 100;
-    } while(k < 10); 
+        k += 2;
+    } while(k < 50); 
     } /* Like the same while but do while
      does action 1 time and then checks condition
      here the condition is: k < 10 */
