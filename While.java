982ce66c3527667350d8d9a5566916538a93d0d5
Vahe <168283825+Svaveop@users.noreply.github.com>
2024-07-14 21:27:25 +0400

    Create While.java

---
+++ While.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+    int i = 0;
+     while (i <= 10){
+         System.out.println("Element: " + i);
+         i++;
+     }
+    }
+}
