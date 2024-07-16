a2d6b2ce9c8aea31f2047282f026d6ed5eb90832
Vahe <168283825+Svaveop@users.noreply.github.com>
2024-07-16 10:20:35 +0400

    Update SimpleCalculator.java

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -6,47 +6,50 @@ public class Main {
         System.out.print("Enter 1st number: ");
         int num1 = scanner.nextInt();
 
-        System.out.print("Enter 2nd number: ");
-        int num2 = scanner.nextInt();
-
-        int res;
-
         System.out.print("Action: ");
         String action = scanner.nextLine();
         action = scanner.nextLine();
 
+        System.out.print("Enter 2nd number: ");
+        int num2 = scanner.nextInt();
+
+        float res;
+
         switch (action) {
             case "+":
                 res = num1 + num2;
-                System.out.println("Answer: " + res);
+                System.out.print("Answer: " + res);
                 break;
             case "-":
                 res = num1 - num2;
-                System.out.println("Answer: " + res);
+                System.out.print("Answer: " + res);
                 break;
             case "*":
                 res = num1 * num2;
-                System.out.println("Answer: " + res);
+                System.out.print("Answer: " + res);
                 break;
             case "/":
                 if (num2 == 0){
-                    System.out.println("Can't divide by 0");
+                    System.out.print("Can't divide by 0");
                 }else {
-                    res = num1 / num2;
-                    System.out.println("Answer: " + res);
+                    res = (float) num1 / num2;
+                    System.out.print("Answer: " + res);
                 }
                 break;
-                // and if you write "/" like ":"
+            // and if you write "/" like ":"
             case ":":
                 if (num2 == 0){
                     System.out.println("Can't divide by 0");
                 }else {
-                    res = num1 / num2;
+                    res = (float) num1 / num2;
                     System.out.println("Answer: " + res);
                 }
                 break;
             default:
                 System.out.println("You did something wrong");
+
+
         }
+
     }
 }
