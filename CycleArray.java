8d0d20b964bbda13fb99880e12f159fc73958f42
Vahe <168283825+Svaveop@users.noreply.github.com>
2024-07-27 21:42:19 +0400

    Create CycleArray.java

---
+++ CycleArray.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        float[] nums2 = new float[] {5.0f, 6.445f, 89.9f};
+        System.out.println(nums2[1]);
+
+        for(int i = 0; i < nums2.length; i++){
+            System.out.println("Element: " + nums2[i]);
+        }
+    }
+}
